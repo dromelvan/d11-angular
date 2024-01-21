a99ddbfc99af32b39cff889752089667e32b38f4
dromelvan <dromelvan@aland.net>
2024-01-21 15:09:14 +0200

    Match Tailwind breakpoints to Angular Material.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
     content: ["./src/**/*.{html,ts}"],
     theme: {
+        screens: {
+            // Match breakpoints to Angular Material
+            xs: { max: "599px" },
+            sm: { min: "600px", max: "959px" },
+            md: { min: "960px", max: "1279px" },
+            lg: { min: "1280px", max: "1919px" },
+            xl: { min: "1920px" },
+        },
         container: {
             center: true,
         },
