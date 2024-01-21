ccc5a9d0bc7b823425b4bb8b890a4770f1a23515
dromelvan <dromelvan@aland.net>
2024-01-21 15:39:02 +0200

    Added D11 button component.

---
+++ src/app/component/d11-button/d11-button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+    selector: 'd11-button',
+    standalone: true,
+    templateUrl: './d11-button.component.html',
+    imports: [MatButtonModule],
+})
+export class D11ButtonComponent {}
