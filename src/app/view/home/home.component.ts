c1a149fcae7b6b3f4ca6a5c03a82673fbcacd009
dromelvan <dromelvan@aland.net>
2024-02-02 21:41:03 +0200

    Tweaked home component

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { D11ButtonComponent } from '../../component/d11-button/d11-button.component';
 import { BreakpointService } from '../../shared/services/breakpoint.service';
-import { NgIf } from '@angular/common';
 
 @Component({
     selector: 'd11-home',
-    imports: [D11ButtonComponent, NgIf],
+    imports: [D11ButtonComponent],
     standalone: true,
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
