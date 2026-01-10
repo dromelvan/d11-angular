eab63c0af7a4da5c3be2e9ab60f3ea3fda6d0407
dromelvan <dromelvan@users.noreply.github.com>
2026-01-11 00:39:36 +0200

    test: Angular Test Library + Vitest

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,23 +1,11 @@
-import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { render, screen } from '@testing-library/angular';
+import { expect } from 'vitest';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App],
-    }).compileComponents();
-  });
+  test('Hello is visible', async () => {
+    await render(App);
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, d11-angular');
+    expect(screen.getByText('Hello, d11-angular')).toBeVisible();
   });
 });
---
+++ vitest-setup.ts
@@ -0,0 +1,2 @@
+// Needed for using Angular Testing Library with Vitest
+import '@testing-library/jest-dom/vitest';
