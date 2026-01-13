ce085fb2d07705d322fa861855716a9d89c02bcb
dromelvan <dromelvan@users.noreply.github.com>
2026-01-13 18:42:29 +0200

    refactor: Index.ts for form components

---
+++ src/app/shared/form/index.ts
@@ -0,0 +1,4 @@
+export * from './button-submit/button-submit.component';
+export * from './checkbox/checkbox.component';
+export * from './input-text/input-text.component';
+export * from './input-password/input-password.component';
