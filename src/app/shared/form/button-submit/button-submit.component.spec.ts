95543d6717377041b2cb1fe6e86d788d589ab866
dromelvan <dromelvan@users.noreply.github.com>
2026-01-13 10:26:36 +0200

    feat: Progress spinner on submit button

--- src/app/shared/form/button-submit/button-submit.component.spec.ts
+++ src/app/shared/form/button-submit/button-submit.component.spec.ts
@@ -1,29 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { render, screen } from '@testing-library/angular';
 import userEvent from '@testing-library/user-event';
 import { ButtonSubmitComponent } from './button-submit.component';
+import { expect } from 'vitest';
+import { waitFor, within } from '@testing-library/dom';
 
 const PROPERTY = 'property';
-const SUBMIT = 'Submit';
-const ICON_SUBMIT = 'iconSubmit';
+const LABEL = 'label';
 const ICON = 'pi-icon';
+const LABEL_NO_ICON = 'labelWorking';
 
 @Component({
   template: `
     <form [formGroup]="form">
       <input formControlName="${PROPERTY}" id="${PROPERTY}" />
-      <app-button-submit [form]="form" label="${SUBMIT}" (click)="onSubmit()" />
-      <app-button-submit [form]="form" label="${ICON_SUBMIT}" icon="${ICON}" />
+      <app-button-submit
+        [form]="form"
+        label="${LABEL}"
+        icon="${ICON}"
+        [working]="working"
+        (click)="onSubmitWork()"
+      />
+      <app-button-submit [form]="form" label="${LABEL_NO_ICON}" (click)="onSubmit()" />
     </form>
   `,
   standalone: true,
   imports: [ReactiveFormsModule, ButtonSubmitComponent],
 })
 class HostComponent {
+  working = signal(false);
+
   form = new FormGroup({
     [PROPERTY]: new FormControl('', Validators.required),
   });
+
+  onSubmitWork() {
+    this.working.set(true);
+    this.onSubmit();
+  }
 
   onSubmit() {
     this.form.reset(this.form.getRawValue());
@@ -31,58 +46,112 @@ class HostComponent {
 }
 
 describe('ButtonSubmitComponent', () => {
-  let form: FormGroup;
+  let component: HostComponent;
   let input: HTMLInputElement;
   let button: HTMLButtonElement;
+  let buttonNoIcon: HTMLButtonElement;
   let user: ReturnType<typeof userEvent.setup>;
 
   beforeEach(async () => {
     const { fixture } = await render(HostComponent);
 
-    form = fixture.componentInstance.form;
+    component = fixture.componentInstance;
     input = screen.getByRole('textbox');
-    button = screen.getByRole('button', { name: SUBMIT });
+    button = screen.getByRole('button', { name: ICON + LABEL });
+    buttonNoIcon = screen.getByRole('button', { name: LABEL_NO_ICON });
     user = userEvent.setup();
   });
 
   it('renders', async () => {
     expect(button).toBeInTheDocument();
     expect(button).toBeDisabled();
-    expect(button).toHaveTextContent(SUBMIT);
-  });
-
-  it('does not render icon when icon is not provided', async () => {
-    expect(button.querySelector('.' + ICON)).not.toBeInTheDocument();
+    expect(button).toHaveTextContent(LABEL);
   });
 
   it('renders icon when icon is provided', async () => {
-    const iconButton = screen.getByRole('button', { name: ICON_SUBMIT });
-
-    expect(iconButton.querySelector('.' + ICON)).toBeInTheDocument();
+    expect(within(button).getByText(ICON)).toBeInTheDocument();
   });
 
-  it('updates button state on form state change', async () => {
+  it('does not render icon when icon is not provided', async () => {
+    expect(within(buttonNoIcon).queryByText(ICON)).not.toBeInTheDocument();
+  });
+
+  it('renders spinner when working and working is provided', async () => {
+    expect(within(button).queryByRole('progressbar')).not.toBeInTheDocument();
+
+    await user.type(input, 'userInput');
+    await user.click(button);
+
+    expect(within(button).getByRole('progressbar')).toBeInTheDocument();
+
+    component.working.set(false);
+
+    await waitFor(() => {
+      expect(within(button).queryByRole('progressbar')).not.toBeInTheDocument();
+    });
+  });
+
+  it('does not render label and icon when working and working is provided', async () => {
+    expect(within(button).getByText(LABEL)).toBeInTheDocument();
+    expect(within(button).getByText(ICON)).toBeInTheDocument();
+
+    await user.type(input, 'userInput');
+    await user.click(button);
+
+    expect(within(button).queryByText(LABEL)).not.toBeInTheDocument();
+    expect(within(button).queryByText(ICON)).not.toBeInTheDocument();
+
+    component.working.set(false);
+
+    await waitFor(() => {
+      expect(within(button).getByText(LABEL)).toBeInTheDocument();
+      expect(within(button).getByText(ICON)).toBeInTheDocument();
+    });
+  });
+
+  it('disables when form is not dirty and valid', async () => {
+    const form = component.form;
+
     expect(form.valid).toBe(false);
     expect(form.dirty).toBe(false);
-    expect(button).toBeDisabled();
+    expect(buttonNoIcon).toBeDisabled();
 
     await user.type(input, 'userInput');
 
     expect(form.valid).toBe(true);
     expect(form.dirty).toBe(true);
-    expect(button).toBeEnabled();
+    expect(buttonNoIcon).toBeEnabled();
 
     await user.clear(input);
 
     expect(form.valid).toBe(false);
     expect(form.dirty).toBe(true);
-    expect(button).toBeDisabled();
+    expect(buttonNoIcon).toBeDisabled();
 
     await user.type(input, 'userInput');
-    await user.click(button);
+    await user.click(buttonNoIcon);
 
     expect(form.valid).toBe(true);
     expect(form.dirty).toBe(false);
+    expect(buttonNoIcon).toBeDisabled();
+  });
+
+  it('disables when working and working is provided', async () => {
     expect(button).toBeDisabled();
+
+    await user.type(input, 'userInput');
+    expect(button).toBeEnabled();
+
+    await user.click(button);
+    expect(button).toBeDisabled();
+
+    await user.type(input, 'userInput');
+    expect(button).toBeDisabled();
+
+    component.working.set(false);
+
+    await waitFor(() => {
+      expect(button).toBeEnabled();
+    });
   });
 });
--- src/app/shared/form/button-submit/button-submit.component.ts
+++ src/app/shared/form/button-submit/button-submit.component.ts
@@ -1,14 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, WritableSignal } from '@angular/core';
 import { ButtonDirective, ButtonIcon, ButtonLabel } from 'primeng/button';
 import { FormGroup } from '@angular/forms';
+import { ProgressSpinner } from 'primeng/progressspinner';
 
 @Component({
   selector: 'app-button-submit',
-  imports: [ButtonDirective, ButtonIcon, ButtonLabel],
+  imports: [ButtonDirective, ButtonIcon, ButtonLabel, ProgressSpinner],
   templateUrl: './button-submit.component.html',
+  styleUrl: './button-submit.component.css',
 })
 export class ButtonSubmitComponent {
   @Input() form!: FormGroup;
   @Input() label!: string;
   @Input() icon?: string;
+  @Input() working?: WritableSignal<boolean>;
 }
