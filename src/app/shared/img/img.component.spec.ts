019d175656e4c26cbc860d306a99e6d978544fa3
dromelvan <dromelvan@users.noreply.github.com>
2026-02-22 10:57:47 +0200

    feat: Priority property for image component

--- src/app/shared/img/img.component.spec.ts
+++ src/app/shared/img/img.component.spec.ts
@@ -17,12 +17,21 @@ class HostComponent {
   src = src;
 }
 
-describe('ImgComponent', () => {
-  beforeEach(async () => {
-    await render(HostComponent, {});
-  });
+@Component({
+  template: ` <app-img [size]="size" [alt]="alt" [src]="src" priority /> `,
+  standalone: true,
+  imports: [ImgComponent],
+})
+class HostPriorityComponent {
+  size = size;
+  alt = alt;
+  src = src;
+}
 
+describe('ImgComponent', () => {
   it('renders', async () => {
+    await render(HostComponent);
+
     const img = screen.getByRole('img');
 
     expect(img).toBeInTheDocument();
@@ -31,5 +40,21 @@ describe('ImgComponent', () => {
     expect(img).toHaveAttribute('width', `${size}`);
     expect(img).toHaveAttribute('height', `${size}`);
     expect(img).toHaveStyle({ height: `${size}px` });
+  });
+
+  it('has fetchPriority auto when priority is false', async () => {
+    await render(HostComponent);
+
+    const img = screen.getByRole('img');
+
+    expect(img).toHaveAttribute('fetchPriority', 'auto');
+  });
+
+  it('has fetchPriority high when priority is true', async () => {
+    await render(HostPriorityComponent);
+
+    const img = screen.getByRole('img');
+
+    expect(img).toHaveAttribute('fetchPriority', 'high');
   });
 });
--- src/app/shared/img/img.component.ts
+++ src/app/shared/img/img.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { booleanAttribute, Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -10,4 +10,5 @@ export class ImgComponent {
   @Input() src!: string;
   @Input() size!: number;
   @Input() alt!: string;
+  @Input({ transform: booleanAttribute }) priority = false;
 }
