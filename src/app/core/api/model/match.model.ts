a11cd6dbf217f45ab1723f75040d4af272967e55
dromelvan <dromelvan@users.noreply.github.com>
2026-03-14 14:01:50 +0200

    refactor: Match model

--- src/app/core/api/model/match.model.ts
+++ src/app/core/api/model/match.model.ts
@@ -1,21 +1,10 @@
-import { Status } from './status.model';
-import { TeamBase } from './team-base.model';
-import { MatchWeekBase } from './match-week-base.model';
 import { GoalBase } from './goal-base.model';
+import { MatchBase } from '@app/core/api';
 
-export interface Match {
-  id: number;
+export interface Match extends MatchBase {
   statSourceId: number;
-  datetime: string;
-  homeTeamGoalsScored: number;
-  awayTeamGoalsScored: number;
   previousHomeTeamGoalsScored: number;
   previousAwayTeamGoalsScored: number;
-  elapsed: string;
-  status: Status;
-  homeTeam: TeamBase;
-  awayTeam: TeamBase;
-  matchWeek: MatchWeekBase;
 
   homeTeamGoals?: GoalBase[];
   awayTeamGoals?: GoalBase[];
