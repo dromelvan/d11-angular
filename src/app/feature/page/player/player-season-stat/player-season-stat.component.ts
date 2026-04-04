8c5971738b393b8e71535d6f17956d3885ea641b
dromelvan <dromelvan@users.noreply.github.com>
2026-04-04 21:38:24 +0300

    chore: Remove unused import

--- src/app/feature/page/player/player-season-stat/player-season-stat.component.ts
+++ src/app/feature/page/player/player-season-stat/player-season-stat.component.ts
@@ -3,20 +3,11 @@ import { Component, input } from '@angular/core';
 import { PlayerSeasonStat } from '@app/core/api';
 import { FeePipe } from '@app/shared/pipes';
 import { RatingPipe } from '@app/shared/pipes/rating.pipe';
-import { AvatarComponent } from '@app/shared/avatar/avatar.component';
 import { D11TeamBaseComponent, TeamBaseComponent } from '@app/shared/resource';
 
 @Component({
   selector: 'app-player-season-stat',
-  imports: [
-    NgClass,
-    FeePipe,
-    RatingPipe,
-    DecimalPipe,
-    AvatarComponent,
-    TeamBaseComponent,
-    D11TeamBaseComponent,
-  ],
+  imports: [NgClass, FeePipe, RatingPipe, DecimalPipe, TeamBaseComponent, D11TeamBaseComponent],
   templateUrl: './player-season-stat.component.html',
 })
 export class PlayerSeasonStatComponent {
